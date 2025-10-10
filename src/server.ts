2b4753fb99e31f6343ca73d49f3323af62e50974
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-10-10 20:56:07 -0300

    feat: Configurando o server.ts

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import fastify from "fastify";
+import "express-async-errors"
+
+const app = fastify()
+
+app.get('/', async (request, reply) => {
+    return reply.status(201).send('Sucesso!')
+})
+
+app.post('/', async (request, reply) => {
+    return reply.status(201).send('Sucesso!')
+})
+
+app.put('/', async (request, reply) => {
+    return reply.status(201).send('Sucesso!')
+})
+
+app.delete('/', async (request, reply) => {
+    return reply.status(201).send('Sucesso.')
+})
+
+
+
+app.listen({ port: 3333 }).then(() => {
+    console.log("HTTP server running")
+})
