ec42c0c5687b160fd4f4d234891cf57af05b6857
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-10-11 16:07:34 -0300

    feat: Criando funcionalidade de criar usuário.

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,2 +1,45 @@
 import fastify from "fastify";
+import { prisma } from "../prisma.js";
+import type { User } from "../types/user.js";
+import { compare, hash } from "bcryptjs";
+import { v4 } from "uuid";
 
+const createUser = async (data: User) => {
+    const { name, email, password } = data;
+
+    const userAlreadyExist = await prisma.user.findUnique({ where: { email } });
+
+    if (userAlreadyExist) return null;
+
+    const hashPassword = await hash(password, 10);
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email: email.toLowerCase(),
+            password: hashPassword,
+            role: "user"
+        }
+    });
+
+    if (!user) return null;
+
+    return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt
+    }
+}
+
+// const getUser = (req, res) => { }
+
+// const getUserById = (req, res) => { }
+
+// const updateUser = (req, res) => { }
+
+// const deleteUser = (req, res) => { }
+
+export { createUser }
