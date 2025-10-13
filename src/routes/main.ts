0bb3e70cb246b908d7591507c85eed2315926ebb
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-10-12 23:39:47 -0300

    feat: Adicionando e modificando arquivo de rotas.

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,2 +1,15 @@
 import fastify from "fastify";
+import { createUser } from "../controllers/userController.js";
 
+const routes = fastify();
+
+routes.get('/', async (req, res) => {
+    const data = {
+        name: "GymPlanner API",
+        version: "1.0.0",
+        timestamp: new Date().toISOString(),
+        status: "success"
+    }
+
+    return res.send(data);
+})
