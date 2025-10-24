0a8b0b7aa5bba0d30269a37dc6eb29d94fd44676
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-10-24 20:28:05 -0300

    refactor: Modulando as rotas do sistema para solução do bug de not found para rotas.

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,16 @@ import fastify from "fastify";
 import "express-async-errors"
 import fastifyCors from "@fastify/cors";
 import fastifyHelmet from "@fastify/helmet";
+import { userRouter } from "./routes/userRoutes/createUser.js";
+import { globalRouter } from "./routes/routes.js";
 
 const server = fastify()
 
 server.register(fastifyHelmet)
 server.register(fastifyCors)
+
+server.register(globalRouter);
+server.register(userRouter);
 
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
