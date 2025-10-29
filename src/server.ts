4f5e4220bf5963584e8bd5a171d9ceb33557aac2
Martíniano Gomes <uricanooriental@gmail.com>
2025-10-29 15:24:32 -0300

    feat: Configuração do JWT.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config';
+
 import fastify from "fastify";
+import fastifyJwt from "@fastify/jwt";
 import fastifyCors from "@fastify/cors";
 import fastifyHelmet from "@fastify/helmet";
+
 import { userRoutes } from "./routes/userRoutes.js";
 import { globalRouter } from "./routes/globalRoutes.js";
 
@@ -8,6 +12,15 @@ const server = fastify({ logger: true })
 
 server.register(fastifyHelmet)
 server.register(fastifyCors)
+
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+    server.log.error("CRITICAL ERROR: JWT_SECRET is not defined in the .env file or the .env file was not loaded.")
+    process.exit(1);
+}
+
+server.register(fastifyJwt, { secret: jwtSecret })
 
 server.register(globalRouter);
 server.register(userRoutes, { prefix: '/user' });
