5844fa57d100e0706ffa53abb7738c32f78882f7
Martíniano Gomes <uricanooriental@gmail.com>
2025-10-29 10:47:15 -0300

    feat: Desenvolvendo a funcionalidade de listar todos os usuários e adicionando sua rota.

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,6 +3,7 @@ import * as userController from "../controllers/userController.js";
 
 async function userRoutes(fastify: FastifyInstance, options: FastifyPluginOptions) {
     fastify.post('/register', userController.createUser);
+    fastify.get('/getAllUsers', userController.getAllUsers);
     fastify.patch('/update/:id', userController.updateUser);
 }
 
