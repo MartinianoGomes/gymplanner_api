e448f8e064ecd3a85368d6fccba20520f70e5dd6
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-10-29 23:05:43 -0300

    feat: Desenvolvendo a funcionalidade de autenticação

---
+++ src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+import { getUserByToken } from "../services/userServices.js";
+
+const authHeader = async (request: FastifyRequest, reply: FastifyReply) => {
+    const authHeader = request.headers["authorization"];
+
+    if (!authHeader) {
+        return reply.status(401).send({ error: "Access danied!" });
+    }
+
+    const tokenSplit = authHeader.split("Bearer ");
+
+    if (!tokenSplit[1]) {
+        return reply.status(401).send({ error: "Access danied!" });
+    }
+
+    const token = tokenSplit[1];
+
+    const userToken = await getUserByToken(token);
+
+    if (!userToken) {
+        return reply.status(401).send({ error: "Access danied!" });
+    }
+
+    (reply as any).userToken = userToken;
+}
+
+export { authHeader }
--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,16 +1,32 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
+import { fastify, type FastifyInstance, type FastifyReply, type FastifyRequest } from "fastify";
 import { prisma } from "../lib/prisma.js";
 import { compare } from "bcryptjs";
 import { v4 } from "uuid";
 
-const userLogin = async (email: string, password: string) => {
+const userLogin = async (
+    fastify: FastifyInstance,
+    email: string,
+    password: string
+) => {
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) return null;
 
     const validPassword = await compare(password, user.password);
     if (!validPassword) return null;
 
-    const token = v4();
+    const jti = v4();
+
+    const token = fastify.jwt.sign(
+        {
+            userId: user.id,
+            email: user.email
+        },
+        {
+            sub: user.id,
+            expiresIn: "1d",
+            jti: jti
+        }
+    );
 
     await prisma.user.update({
         where: { email: email },
