cdea76a0471d3f335fdd36fb833196cf4366cabc
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-11-01 18:04:06 -0300

    feat: Importando o plugin cookie e modificando rotas.

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,17 @@ import fastify from "fastify";
 import fastifyJwt from "@fastify/jwt";
 import fastifyCors from "@fastify/cors";
 import fastifyHelmet from "@fastify/helmet";
+import fastifyCookie from '@fastify/cookie';
 
-import { userRoutes } from "./routes/userRoutes.js";
-import { globalRouter } from "./routes/globalRoutes.js";
+import { authRoutes } from "./routes/authRoutes.js";
+import { globalRoutes } from "./routes/globalRoutes.js";
+import { adminRoutes } from './routes/adminRoutes.js';
 
 const server = fastify({ logger: true })
 
 server.register(fastifyHelmet)
 server.register(fastifyCors)
+server.register(fastifyCookie);
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -22,8 +25,9 @@ if (!jwtSecret) {
 
 server.register(fastifyJwt, { secret: jwtSecret })
 
-server.register(globalRouter);
-server.register(userRoutes, { prefix: '/user' });
+server.register(globalRoutes);
+server.register(authRoutes);
+server.register(adminRoutes, { prefix: '/admin' });
 
 const port = Number(process.env.PORT) || 3000;
 
