86d189a7e79dda40d83309f64022b3b335f7e082
Martiniano Gomes <martinianogomes@unifesspa.edu.br>
2025-11-02 17:49:42 -0300

    refactor: Refatorando código.

--- src/controllers/workoutController.ts
+++ src/controllers/workoutController.ts
@@ -1,7 +1,6 @@
 import type { FastifyRequest, FastifyReply } from 'fastify';
 import { prisma } from "../lib/prisma.js";
 import type { Workout } from "../types/workout.js";
-import { v4 } from "uuid";
 
 const createWorkout = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
@@ -32,8 +31,11 @@ const getAllWorkouts = async (_request: FastifyRequest, reply: FastifyReply) => 
 const getWorkoutById = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
     const { id } = request.params as { id: string };
+
     const workout = await prisma.workout.findUnique({ where: { id } });
+
     if (!workout) return reply.status(404).send({ message: "Workout not found." });
+
     return reply.status(200).send(workout);
   } catch (error) {
     return reply.status(500).send({ error, message: "Unable to fetch workout." });
@@ -43,11 +45,12 @@ const getWorkoutById = async (request: FastifyRequest, reply: FastifyReply) => {
 const updateWorkout = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
     const { id } = request.params as { id: string };
+
     const { title, description = null } = request.body as Workout;
 
     const updated = await prisma.workout.update({
       where: { id },
-      data: { 
+      data: {
         title,
         description: description ?? null
       }
@@ -62,7 +65,9 @@ const updateWorkout = async (request: FastifyRequest, reply: FastifyReply) => {
 const deleteWorkout = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
     const { id } = request.params as { id: string };
-    await prisma.workout.delete({ where: { id }});
+
+    await prisma.workout.delete({ where: { id } });
+
     return reply.status(200).send({ message: "Workout deleted successfully!" });
   } catch (error) {
     return reply.status(404).send({ error, message: "Unable to delete the workout. An error occurred." });
---
+++ src/schemas/workoutSchema.ts
@@ -0,0 +1,5 @@
+import z from "zod";
+
+const workoutSchema = z.object({})
+
+export { workoutSchema };
