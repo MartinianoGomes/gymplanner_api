695c6a68c3f613d29d899233ed51c3eedbef292c
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-11-22 22:05:44 -0300

    Update userController.ts

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,13 @@
-import { type FastifyRequest, type FastifyReply, fastify } from 'fastify';
+import { type FastifyRequest, type FastifyReply } from 'fastify';
 import { prisma } from "../lib/prisma.js";
 import type { User } from "../types/user.js";
 import { hash } from "bcryptjs";
 import { login, logout } from '../services/userServices.js';
 import { loginSchema } from '../schemas/loginSchema.js';
 import { userSchema } from '../schemas/userSchema.js';
+import { getUserById } from './adminController.js';
 
-const createUser = async (request: FastifyRequest, reply: FastifyReply) => {
+export const createUser = async (request: FastifyRequest, reply: FastifyReply) => {
     const validateUserInformations = userSchema.safeParse(request.body as User);
     if (!validateUserInformations.success) return reply.status(400).send({ error: "Invalid user informations." })
 
@@ -29,90 +30,19 @@ const createUser = async (request: FastifyRequest, reply: FastifyReply) => {
         id: user.id,
         name: user.name,
         email: user.email,
-        role: user.role,
+        role: user.role?.toUpperCase(),
         createdAt: user.createdAt,
         updatedAt: user.updatedAt
     }
 }
 
-const updateUser = async (request: FastifyRequest, reply: FastifyReply) => {
-    const { id } = request.params as { id: string }
-
-    if (!id) return reply.status(400).send({ error: "Please provide the user ID." })
-
-    const validateUserInformations = userSchema.partial().safeParse(request.body as User);
-    if (!validateUserInformations.success) return reply.status(400).send({ error: "Invalid user informations." })
-
-    const { name, email, password } = validateUserInformations.data;
-
-    const updateData: {
-        name?: string;
-        email?: string;
-        password?: string;
-    } = {};
-
-    if (name) updateData.name = name;
-
-    if (email) updateData.email = email.toLocaleLowerCase();
-
-    if (password) updateData.password = await hash(password, 10);;
-
-    try {
-        const userUpdated = await prisma.user.update({
-            where: { id: id },
-            data: updateData
-        })
-
-        const { password, ...userWithoutPassword } = userUpdated;
-
-        return userWithoutPassword;
-    } catch (error) {
-        return reply.status(404).send({ error: "User not found!" });
-    }
-}
-
-const getAllUsers = async (request: FastifyRequest, reply: FastifyReply) => {
-    try {
-        const users = await prisma.user.findMany({
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                role: true,
-                createdAt: true,
-                updatedAt: true
-            }
-        });
-
-        return users;
-    } catch (error) {
-        return reply.status(404).send({ error: "Users not found." });
-    }
-}
-
-const deleteUser = async (request: FastifyRequest, reply: FastifyReply) => {
-    const { id } = request.params as { id: string };
-
-    if (!id) return reply.status(404).send({ error: "User not found." });
-
-    try {
-        await prisma.user.delete({
-            where: { id }
-        })
-
-        return reply.status(200).send({ message: "User deleted successfully!" });
-    } catch (error) {
-        return reply.status(404).send({ error: "Unable to delete the user. An error occurred." });
-    }
-}
-
-const userLogin = async (request: FastifyRequest, reply: FastifyReply) => {
+export const userLogin = async (request: FastifyRequest, reply: FastifyReply) => {
     const validateCredentials = loginSchema.safeParse(request.body as User);
     if (!validateCredentials.success) return reply.status(400).send({ error: "Invalid credentials." })
 
     const { email, password } = validateCredentials.data;
 
-    const token = await login(request.server, email, password);
+    const token = await login(request.server, email.toLowerCase(), password);
     if (!token) return reply.status(401).send({ error: "Incorrect credentials." });
 
     return reply
@@ -126,13 +56,99 @@ const userLogin = async (request: FastifyRequest, reply: FastifyReply) => {
         .send({ message: "User logged in successfully." });
 }
 
-const userLogout = async (request: FastifyRequest, reply: FastifyReply) => {
+export const userLogout = async (request: FastifyRequest, reply: FastifyReply) => {
     const token = request.cookies.token;
-    if (!token) return reply.status(401).send({ error: "User not logged in." });
+    if (!token) {
+        await logout(reply);
+        return reply.status(401).send({ error: "User not logged in." });
+    }
 
     await logout(reply);
 
     return reply.status(200).send({ message: "User logged out successfully." });
 }
 
-export { createUser, updateUser, getAllUsers, deleteUser, userLogin, userLogout }
+export const getMe = async (request: FastifyRequest, reply: FastifyReply) => {
+    const userId = request.user?.userId;
+    if (!userId) {
+        await logout(reply);
+        return reply.status(401).send({ error: "Authentication required." });
+    }
+
+    try {
+        const user = await getUserById(userId);
+        if (!user) {
+            await logout(reply);
+            return reply.status(404).send({ error: "User not found." });
+        };
+
+        return reply.status(200).send(user);
+    } catch (error) {
+        return reply.status(500).send({ error: "Internal server error." });
+    }
+}
+
+export const updateProfile = async (request: FastifyRequest, reply: FastifyReply) => {
+    const userId = request.user?.userId;
+    if (!userId) {
+        await logout(reply);
+        return reply.status(401).send({ error: "Authentication required." });
+    }
+
+    const validate = userSchema.partial().safeParse(request.body);
+    if (!validate.success) {
+        await logout(reply);
+        return reply.status(400).send({ error: "Invalid user information." });
+    }
+
+    const { name, email, password } = validate.data;
+
+    const updateData: {
+        name?: string;
+        email?: string;
+        password?: string;
+    } = {};
+
+    if (name) updateData.name = name;
+    if (email) updateData.email = email.toLowerCase();
+    if (password) updateData.password = await hash(password, 10);
+
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: updateData,
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                role: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        });
+
+        return reply.status(200).send(updatedUser);
+    } catch (error) {
+        return reply.status(400).send({ error: "Unable to update profile." });
+    }
+}
+
+export const deleteProfile = async (request: FastifyRequest, reply: FastifyReply) => {
+    const userId = request.user?.userId;
+    if (!userId) {
+        await logout(reply);
+        return reply.status(401).send({ error: "Authentication required." });
+    }
+
+    try {
+        await prisma.user.delete({
+            where: { id: userId }
+        });
+
+        await logout(reply);
+
+        return reply.status(200).send({ message: "User profile deleted successfully." });
+    } catch (error) {
+        return reply.status(400).send({ error: "Unable to delete profile." });
+    }
+}
