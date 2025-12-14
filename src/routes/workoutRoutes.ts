85826e7037be6e3941b3af9e037f130deee2811c
Martiniano Gomes <148648246+MartinianoGomes@users.noreply.github.com>
2025-12-14 01:01:21 -0300

    Update workoutRoutes.ts

--- src/routes/workoutRoutes.ts
+++ src/routes/workoutRoutes.ts
@@ -1,11 +1,15 @@
 import type { FastifyInstance, FastifyPluginOptions } from "fastify";
 import * as workoutController from "../controllers/workoutController.js";
+import { verifyJwt } from "../hooks/authHooks.js";
 
 export async function workoutRoutes(fastify: FastifyInstance) {
     fastify.register(workoutModule, { prefix: "/workout" });
 }
 
 async function workoutModule(fastify: FastifyInstance, options: FastifyPluginOptions) {
+    // Aplica verificação de JWT em todas as rotas de workout
+    fastify.addHook("onRequest", verifyJwt);
+
     fastify.post('/create', workoutController.createWorkout);
     fastify.get('/', workoutController.getMyWorkouts);
     fastify.get('/:id', workoutController.getWorkoutById);
